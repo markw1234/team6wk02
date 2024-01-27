87d9f361206e82a0a14955f3b5acbdee36079152
joao <jmoita@fusionware.com>
2024-01-26 20:39:53 -0700

    Made website changes

---
+++ src/js/productDetails.mjs
@@ -0,0 +1,34 @@
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
+
+let product = {};
+
+export default async function productDetails(productId) {
+
+    product = await findProductById(productId);
+
+    renderProductDetails();
+
+    document
+        .getElementById("addToCart")
+        .addEventListener("click", addToCart);
+
+}
+
+function addToCart() {
+    setLocalStorage("so-cart", product);
+  }
+
+  function renderProductDetails() {
+    document.querySelector("#productTitle").innerText = product.Brand.Name;
+    // document.querySelector("#productNameWithoutBrand").innerText =
+    //   product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText =
+      product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+      product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
+  }
