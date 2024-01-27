b0522fde9f583e89961cc14d864ca80fece980c3
joao <jmoita@fusionware.com>
2024-01-26 20:57:20 -0700

    added public folder

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -4,11 +4,11 @@ import { setLocalStorage } from "./utils.mjs";
 let product = {};
 
 export default async function productDetails(productId) {
-
+    // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     product = await findProductById(productId);
-
+     // once we have the product details we can render out the HTML
     renderProductDetails();
-
+    // add a listener to Add to Cart button 
     document
         .getElementById("addToCart")
         .addEventListener("click", addToCart);
